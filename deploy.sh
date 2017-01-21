246e6b8734640c25d751fbbcf9453d9aba84945e
Stuart Mashaal <stumash1@gmail.com>
2017-01-21 11:11:32 -0500

    initconfig now working

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 
 mvn clean install &&
 
-scp target/courseplanner david@138.197.6.26:/opt/tomcat/webapps/courseplanner
+scp target/courseplanner.war david@138.197.6.26:/opt/tomcat/webapps/courseplanner.war
+
