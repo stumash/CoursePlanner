c920e07b93e5119d55af5cdba24ce73e33ab390c
David Huculak <davidhuculak5@gmail.com>
2017-01-27 12:18:43 -0500

    revamped deploy script so that it makes the html perform a console.log indicating the commit they were built off of

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# get ID of current commit
+commitHash=$(git rev-parse HEAD) &&
+
+# add a script to html files that logs the current commit
+sed -i "3i\<script> console.log(\"This site was built against the source of the following commit: $commitHash\") </script> " ./src/main/webapp/index.html &&
+sed -i "4i\<script> console.log(\"This site was built against the source of the following commit: $commitHash\") </script> " ./src/main/webapp/scheduleBuilder.html &&
+
+# build project
 mvn clean install &&
 
+# remove commit logging script from html
+# we do not want our deploy script to change our source files
+sed -i "3d" ./src/main/webapp/index.html &&
+sed -i "4d" ./src/main/webapp/scheduleBuilder.html &&
+
+# transfer the built project onto the VM
+# this will trigger Tomcat to reload the site content
 scp target/courseplanner.war david@138.197.6.26:/opt/tomcat/webapps/courseplanner.war
