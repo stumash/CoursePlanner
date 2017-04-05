208465d7075f56e5c99980df454a580dbe611a09
David Huculak <davidhuculak5@gmail.com>
2017-04-05 00:12:49 -0400

    another scrape fix

--- webscraping/node/scrapeTheWeb.sh
+++ webscraping/node/scrapeTheWeb.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-COURSE_PLANNER_HOME="/opt/tomcat/webapps/courseplanner" node /home/david/CoursePlanner/webscraping/node/driver.js -v
+cd /home/david/CoursePlanner/webscraping/node/
+COURSE_PLANNER_HOME="/opt/tomcat/webapps/courseplanner" node driver.js -v
