ede96dcddcd435e4c2c6f3d5d985ff4d5f2d2243
David Huculak <davidhuculak5@gmail.com>
2017-04-04 23:47:19 -0400

    fix web scraping shell script to be able to run in any directory

--- webscraping/node/scrapeTheWeb.sh
+++ webscraping/node/scrapeTheWeb.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-COURSE_PLANNER_HOME="/opt/tomcat/webapps/courseplanner" node driver.js -v > ~/CoursePlannerScraping.log
+COURSE_PLANNER_HOME="/opt/tomcat/webapps/courseplanner" node /home/david/CoursePlanner/webscraping/node/driver.js -v
