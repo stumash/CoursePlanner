4ce71301297ac1823dcd76f288f06ff46d78de68
David Huculak <davidhuculak5@gmail.com>
2017-04-21 15:10:18 -0400

    moved sequence scraper stuff into its own folder

--- webscraping/node/scrapeTheWeb.sh
+++ webscraping/node/scrapeTheWeb.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cd /home/david/CoursePlanner/webscraping/node/
+cd /home/david/CoursePlanner/webscraping/node/course-seq-scraper
 COURSE_PLANNER_HOME="/opt/tomcat/webapps/courseplanner" node driver.js -v
