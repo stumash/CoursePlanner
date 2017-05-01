cfe44cd83bcb6a43724ba8ab2e58c4ae4cc5dcf3
Stuart Mashaal <stumash1@gmail.com>
2017-05-01 00:53:33 -0400

    can now scrape course data from urls in text file
    scrape-course-data.r reads faculty names, urls, and css-selectors
    from the file url_css-selector_pairs.txt. These triples (name, url,
    css-selector) define an online source of Concordia course data to scrape
    from.  The R code scrapes course data from each data source defined in
    the text file in a loop and prints all tables it generates to
    '#'-delimited csv files.
    
    Signed-off-by: Stuart Mashaal <stumash1@gmail.com>

--- webscraping/r/scrape-course-data.r
+++ webscraping/r/scrape-course-data.r
@@ -11,76 +11,78 @@ program.names <- urls.and.css.selectors[seq(from = 1, to = length(urls.and.css.s
 urls <- urls.and.css.selectors[seq(from = 2, to = length(urls.and.css.selectors), by = 3)]
 css.selectors <- urls.and.css.selectors[seq(from = 3, to = length(urls.and.css.selectors), by = 3)]
 
-# we want to do the rest of the file inside a loop for as
-# many scrapes as defined in num.scrapes
+for(i in 1:num.scrapes) {
+    htmlpage.string <- read_html(urls[i]) # get the raw html
+    program.html.string <- htmlpage.string %>%
+        html_node(css = css.selectors[i]) # extract desired DOM
 
-htmlpage.string <- read_html(urls[1]) # get the raw html
-program.html.string <- htmlpage.string %>%
-  html_node(css = css.selectors[1]) # extract desired DOM
+    # match "course info header" of each course on concordia web site
+    # ...something like 'SOEN 555    Systems'
+    course.info.header <- "[A-Z]{4} [0-9]{3}[[:space:]]+?[A-Z][a-z]+."
 
-# match "course info header" of each course on concordia web site
-# ...something like 'SOEN 555    Systems'
-course.info.header <- "[A-Z]{4} [0-9]{3}[[:space:]]+?[A-Z][a-z]+."
+    # split string on empty string before each course.info.header, convert to data frame
+    program.courses <- html_text(program.html.string) %>%
+        str_split( paste(sep = "", "(?=(", course.info.header, "))") )
 
-# split string on empty string before each course.info.header, convert to data frame
-program.courses <- html_text(program.html.string) %>%
-  str_split( paste(sep = "", "(?=(", course.info.title, "))") )
+    # list to data frame nonsense
+    program.courses <- program.courses %>% .[[1]] %>% .[-1] %>%
+        as.data.frame(stringsAsFactors = FALSE)
+    colnames(program.courses) <- c("fullstring") # also better column name
 
-# list to data frame nonsense
-program.courses <- program.courses %>% .[[1]] %>% .[-1] %>%
-  as.data.frame(stringsAsFactors = FALSE)
-colnames(program.courses) <- c("fullstring") # also better column name
+    # some cleaning of the full string of each course
+    program.courses[,1] <- str_trim(program.courses[,1])
 
-# some cleaning of the full string of each course
-program.courses[,1] <- str_trim(program.courses[,1])
+    # some regexes for data extraction
+    course.code.regex <- '[A-Z]{4} [0-9]{3}'
+    course.name.regex <- '[A-Z][a-z][^(]*'
+    credits.regex <- '\\(([0-9](\\.[0-9])?[0-9]?) credits\\)'
+    prereqs.regex <- 'Prerequisite: ([^.]*)'
 
-# some regexes for data extraction
-course.code.regex <- '[A-Z]{4} [0-9]{3}'
-course.name.regex <- '[A-Z][a-z][^(]*'
-credits.regex <- '\\(([0-9](\.[0-9])?[0-9]?) credits\\)'
-prereqs.regex <- 'Prerequisite: ([^.]*)'
+    # extracting the regexes to new columns in the program.courses data frame
+    program.courses <- program.courses %>% # str_extract only takes first occurence of regex
+        mutate(course.code = str_extract(program.courses$fullstring, course.code.regex))
+    program.courses <- program.courses %>%
+        mutate(course.name = str_extract(program.courses$fullstring, course.name.regex) %>% str_trim)
+    program.courses <- program.courses %>%
+        mutate(credits = str_match(program.courses$fullstring, credits.regex) %>% .[,2])
+    program.courses <- program.courses %>%
+        mutate(prereq.string = program.courses$fullstring %>% str_match(prereqs.regex) %>% .[,2])
+    program.courses <- program.courses %>%
+        mutate(secondhalf.string = program.courses$fullstring %>% str_split('\\. ', n = 2) %>%
+               sapply(function(x) x[2]) %>% str_trim)
 
-# extracting the regexes to new columns in the program.courses data frame
-program.courses <- program.courses %>% # str_extract only takes first occurence of regex
-  mutate(course.code = str_extract(program.courses$fullstring, course.code.regex))
-program.courses <- program.courses %>%
-  mutate(course.name = str_extract(program.courses$fullstring, course.name.regex) %>% str_trim)
-program.courses <- program.courses %>%
-  mutate(credits = str_match(program.courses$fullstring, credits.regex) %>% .[,2])
-program.courses <- program.courses %>%
-  mutate(prereq.string = program.courses$fullstring %>% str_match(prereqs.regex) %>% .[,2])
-program.courses <- program.courses %>%
-  mutate(secondhalf.string = program.courses$fullstring %>% str_split('\\. ', n = 2) %>%
-         sapply(function(x) x[2]) %>% str_trim)
+    # remove hyphen followed by newline (concordia website programmers being lazy)
+    program.courses$secondhalf.string <- program.courses$secondhalf.string %>%
+        str_replace('-\n', '')
 
-# remove hyphen followed by newline (concordia website programmers being lazy)
-program.courses$secondhalf.string <- program.courses$secondhalf.string %>%
-  str_replace('-\n', '')
+    # some regexes for data extraction from column 'secondhalf.string'
+    lectures.regex <- 'Lectures: ([^.]*)'
+    tutorials.regex <- 'Tutorial: ([^.]*)'
+    laboratory.regex <- 'Laboratory: ([^.]*)'
+    note.regex <- 'NOTE: (.*)'
+    course.description.regex <- '.*?(?=(Lecture|Tutorial|Laboratory|\nNOTE|$))'
 
-# some regexes for data extraction from column 'secondhalf.string'
-lectures.regex <- 'Lectures: ([^.]*)'
-tutorials.regex <- 'Tutorial: ([^.]*)'
-laboratory.regex <- 'Laboratory: ([^.]*)'
-note.regex <- 'NOTE: (.*)'
-course.description.regex <- '.*?(?=(Lecture|Tutorial|Laboratory|\nNOTE|$))'
+    # extracting the regex to new columns in the program.courses data frame
+    program.courses <- program.courses %>%
+        mutate(course.description = program.courses$secondhalf.string %>% str_extract(course.description.regex))
+    program.courses <- program.courses %>%
+        mutate(lecture.hours = program.courses$secondhalf.string %>% str_match(lectures.regex) %>% .[,2])
+    program.courses <- program.courses %>%
+        mutate(tutorial.hours = program.courses$secondhalf.string %>% str_match(tutorials.regex) %>% .[,2])
+    program.courses <- program.courses %>% 
+        mutate(lab.hours = program.courses$secondhalf.string %>% str_match(laboratory.regex) %>% .[,2])
+    program.courses <- program.courses %>%
+        mutate(note = program.courses$secondhalf.string %>% str_match(note.regex) %>% .[,2])
 
-# extracting the regex to new columns in the program.courses data frame
-program.courses <- program.courses %>%
-  mutate(course.description = program.courses$secondhalf.string %>% str_extract(course.description.regex))
-program.courses <- program.courses %>%
-  mutate(lecture.hours = program.courses$secondhalf.string %>% str_match(lectures.regex) %>% .[,2])
-program.courses <- program.courses %>%
-  mutate(tutorial.hours = program.courses$secondhalf.string %>% str_match(tutorials.regex) %>% .[,2])
-program.courses <- program.courses %>% 
-  mutate(lab.hours = program.courses$secondhalf.string %>% str_match(laboratory.regex) %>% .[,2])
-program.courses <- program.courses %>%
-  mutate(note = program.courses$secondhalf.string %>% str_match(note.regex) %>% .[,2])
+    # store and remove redundant data from program.courses data frame
+    full.course.strings <- program.courses$fullstring %>% as.data.frame(stringsAsFactors = FALSE)
+    # remove columns 'fullstring' and 'secondhalf.string' from program.courses data frame
+    program.courses <- program.courses %>% select(-one_of(c('fullstring')))
+    program.courses <- program.courses %>% select(-one_of(c('secondhalf.string')))
 
-# store and remove redundant data from program.courses data frame
-full.course.strings <- program.courses$fullstring %>% as.data.frame(stringsAsFactors = FALSE)
-program.courses <- program.courses %>% select(-one_of(c('fullstring')))
-program.courses <- program.courses %>% select(-one_of(c('secondhalf.string')))
-
-# store data in csv files
-write_delim(full.course.strings, "aeroeng_full-course-info.csv", append = FALSE, col_names = TRUE, delim = "#")
-write_delim(program.courses, "aeroeng-table.csv", append = FALSE, col_names = TRUE, delim = "#")
+    # store data in csv files
+    write_delim(full.course.strings, paste(sep="_",program.names[i],"full-course-info.csv"),
+                append = FALSE, col_names = TRUE, delim = "#")
+    write_delim(program.courses, paste(sep="_",program.names[i],"table.csv"),
+                append = FALSE, col_names = TRUE, delim = "#")
+}
