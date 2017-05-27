b22d5b79708696f8a51fcf5b1d7821bbcf3b5575
David Huculak <davidhuculak5@gmail.com>
2017-05-27 19:38:24 -0400

    fixed sequence webscraper to make tomcat owner of the generated JSON files

--- webscraping/node/scrapeTheWeb.sh
+++ webscraping/node/scrapeTheWeb.sh
@@ -2,3 +2,6 @@
 
 cd /home/david/CoursePlanner/webscraping/node/
 COURSE_PLANNER_HOME="/opt/tomcat/webapps/courseplanner" node driver.js -v
+
+chown tomcat /opt/tomcat/webapps/courseplanner/sequences/
+chgrp tomcat /opt/tomcat/webapps/courseplanner/sequences/
