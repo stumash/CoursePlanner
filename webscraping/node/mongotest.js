0b41cc1a21ec81afe326dafdbe5bdb856bfcfbf5
David Huculak <davidhuculak5@gmail.com>
2017-05-29 22:02:46 -0400

    added preliminary test files

---
+++ webscraping/node/mongotest.js
@@ -0,0 +1,57 @@
+var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
+
+var findDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+}
+
+var deleteAllDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Find some documents
+  collection.deleteMany({}, function(err, result) {
+    assert.equal(err, null);
+    console.log("Deleted all records");
+    callback(result);
+  });
+
+}
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  insertDocuments(db, function() {
+    findDocuments(db, function() {
+      deleteAllDocuments(db, function() {
+        db.close();
+      });
+    });
+  });
+});
