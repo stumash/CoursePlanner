f790358bd71fc750c171dc582d43124f303544bc
Stuart Mashaal <stumash1@gmail.com>
2017-06-05 01:16:26 -0400

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,37 +1,46 @@
 #!/usr/bin/env bash
 
 # tell the user what's going on
-echo "Building & deploying project..." &&
+echo "Building & deploying project..."
 
 # get ID of current commit
 commitHash=$(git rev-parse HEAD) &&
 commitAuthor=$(git --no-pager show -s --format='%an <%ae>' HEAD) &&
-
 # add a comment to the html that indicates the current commit
 sed -i "4i\<!--This site was built against the source of the following commit: $commitHash ($commitAuthor) -->" ./src/main/webapp/index.html &&
 sed -i "4i\<!--This site was built against the source of the following commit: $commitHash ($commitAuthor) -->" ./src/main/webapp/sequenceBuilder.html &&
+gitTagInserted=true
 
 # build project
 mvn clean install -q &&
+mvnCleanInstallSuccessful=true
 
-# remove commit logging script from html
-# we do not want our deploy script to change our source files
-sed -i "4d" ./src/main/webapp/index.html &&
-sed -i "4d" ./src/main/webapp/sequenceBuilder.html &&
+if gitTagInserted
+then
+    # remove commit logging script from html
+    # we do not want our deploy script to change our source files,
+    # only the deployed version of them
+    sed -i "4d" ./src/main/webapp/index.html &&
+    sed -i "4d" ./src/main/webapp/sequenceBuilder.html &&
+fi
 
-echo "Project build complete. Transferring war file to VM" &&
+if ! mvnCleanInstallSuccessful
+then
+    echo "Project build failed" 1>&2
+    exit 1
+fi
+echo "Project build complete. Transferring war file to VM"
 
 # transfer the built project onto the VM
 # this will trigger Tomcat to reload the site content
-scp target/courseplanner.war david@138.197.6.26:/opt/tomcat/webapps/courseplanner.war
+scp target/courseplanner.war david@138.197.6.26:/opt/tomcat/webapps/courseplanner.war &&
+deploymentSuccessful=true
 
 # if ends with error code 0 (success) then print deployment complete, else deployment failed
-if [ "$?" = "0" ];
+if deploymentSuccessful
 then
-	echo -e "\nDeployment completed at: $(date)"
+    echo -e "\nDeployment completed at: $(date)"
 else
-	sed -i "4d" ./src/main/webapp/index.html &&
-	sed -i "4d" ./src/main/webapp/sequenceBuilder.html &&
-	echo -e "\nDeployment failed at: $(date)" 1>&2
-	exit 1
+    echo -e "\nDeployment failed at: $(date)" 1>&2
+    exit 1
 fi
