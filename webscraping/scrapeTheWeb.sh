b5c55c1c024ba816c9f923dd411d8a8c7c073706
Stuart Mashaal <stumash1@gmail.com>
2017-06-08 20:48:03 -0400

    add scrape-course-data.r to cron job

--- webscraping/scrapeTheWeb.sh
+++ webscraping/scrapeTheWeb.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+webscrapedir=$(pwd)
 
 # run scraper for course sequences
 cd /home/david/CoursePlanner/webscraping/node/course-seq-scraper
 node scrapeAndValidate.js
 
+cd $webscrapedir
 # TODO: run scraper for course data ...
+Rscript r/scrape-course-data.r
