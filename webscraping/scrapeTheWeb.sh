2fe7b3607afcba97a23df3aa98b217b656004c5b
Stuart Mashaal <stumash1@gmail.com>
2017-07-10 23:18:48 -0400

    delete and remake scraped json dir before scrape
    In `scrape-the-web.sh`, delete and remake the directory
    `course-info-jsonfiles` before populating it with files containing
    scraped json

--- webscraping/scrapeTheWeb.sh
+++ webscraping/scrapeTheWeb.sh
@@ -3,14 +3,16 @@
 # This is our script for running our webscraping activites. It it to be run in a cron script.
 # Example: 0 0 * * * sh /home/david/CoursePlanner/webscraping/scrapeTheWeb.sh
 
-pushd `dirname $0` > /dev/null
-webscrapedir=$(pwd)
+pushd `dirname $0` > /dev/null # this script is now in the directory of this file
+webscrapedir=$(pwd) # save this directory as $webscrapedir
 popd > /dev/null
 
 # run scraper for course sequences
 cd "$webscrapedir/node/course-seq-scraper"
 node scrapeAndValidate.js
 
+# delete and remake the directory to hold scraped course-info json files
+rm -r $webscrapedir/r/course-info-jsonfiles; mkdir $webscrapedir/r/course-info-jsonfiles
 # run scraper for course data
 cd "$webscrapedir/r"
 Rscript scrape-course-data.r
