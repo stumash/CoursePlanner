9689b05fa7a47f31388907501cd1199eb9bb51da
Stuart Mashaal <stumash1@gmail.com>
2017-07-25 20:49:16 -0400

    resolve issues 51 and 60

--- deploy.sh
+++ deploy.sh
@@ -2,53 +2,68 @@
 
 # This is the deploy script for the CoursePlanner webapp. To run it you will need to have maven installed and have permissions to scp the war file to the remote VM.
 
-if [ $# -gt 0 ] # need one arg
+# parse arguments and handle bad arguments
+if [ "$#" == "1" ]
 then 
     if [ "${1}" == "prod" ]
     then
         devname=""
+        prod=true
+        echo "deploy chosen: production"
     else
+        echo "deploy chosen: $1 development"
         devname="${1:0:1}"
+        prod=false
     fi
 else
-    echo "deploy.sh takes one arg, the name of the dev, for deploying dev-specific .war files." 1>&2
+    echo "deploy.sh failed"
+    echo "deploy.sh takes one argument: the name of the dev or \"prod\""
     exit 1
 fi
+
+echo ""
 
 # run frontend build tools
-echo "Building frontend..." &&
-if [ "${2}" == "x" ]
+echo "building frontend..."
+if $prod
 then
     # build for production (no react dev tools, optimized performance)
-    npm run build-prod
+    npmCommand="npm run build-prod"
 else
     # build for development (with react dev tools, bad performance)
-    npm run build-dev
-fi &&
-
-# compile backend sources and package frontend assets
-echo "Building backend..." &&
-mvn clean install -q &&
-projectBuildSuccessful=true
-
-if ! $projectBuildSuccessful
+    npmCommand="npm run build-dev"
+fi
+if $npmCommand # run the npm command and react to exit code
 then
-    echo "Project build failed" 1>&2
+    echo "frontend build successful"
+else
+    echo "frontend build failed"
     exit 1
 fi
-echo "Project build complete. Transferring war file to VM"
+
+echo ""
+
+# compile backend sources and package frontend assets
+echo "building backend..."
+if mvn clean install
+then
+    echo "backend build succssful. Transferring war file to VM"
+else
+    echo "backend build failed"
+    exit 1
+fi
+
+echo ""
 
 # transfer the built project onto the VM
 # this will trigger Tomcat to reload the site content
-scp "target/courseplanner.war" david@138.197.6.26:/opt/tomcat/webapps/"courseplanner${devname}.war" &&
-deploymentSuccessful=true
-
-# if ends with error code 0 (success) then print deployment complete, else deployment failed
-if $deploymentSuccessful
+deployToServerCommand="scp ./target/courseplanner.war david@138.197.6.26:/opt/tomcat/webapps/courseplanner${devname}.war"
+echo "copying .war file to server..."
+if $deployToServerCommand # run the deployment command and react to exit code
 then
-    echo -e "\nDeployed courseplanner${devname}.war successfully at: $(date)"
+    echo "copying courseplanner${devname}.war to server successful at: $(date)"
 else
-    echo -e "\nFailed to scp courseplanner${devname}.war at: $(date)"
+    echo "copying courseplanner${devname}.war to server failed at: $(date)"
     exit 1
 fi
 
