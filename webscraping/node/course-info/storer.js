f6498c9ed44e64229d7212810b527dd6c40842aa
Stuart Mashaal <stumash1@gmail.com>
2017-08-12 00:18:51 -0400

    more fixes to storer, some file moving

--- webscraping/node/course-info/storer.js
+++ webscraping/node/course-info/storer.js
@@ -1,74 +1,113 @@
-/**
- * storer.js description:
- *   take the json output of scrape-course-data.r and validate it
- *   using json-schema/ajv.  If the json passes validation, store it
- *   in the mongodb.
- */
+'use strict'
 
-/**
- * imports
- */
-let nodemailer = require('nodemailer');
-let fs = require("fs-extra");
-let MongoClient = require('mongodb').MongoClient;
-let assert = require('assert');
-let argv = require('minimist')(process.argv.slice(2));
+let nodemailer = require('nodemailer')
+let remove = require("remove")
+let fs = require("fs")
+let MongoClient = require('mongodb').MongoClient
+let assert = require('assert')
+let argv = require('minimist')(process.argv.slice(2))
+
+const json_schema_path = 'course-info-json-schema.json'
+const scraped_course_info_folder = '../../r/course-info-json-files'
+const course_info_document_regex = /_document/
+
 let Ajv = require('ajv');
-let ajv = new Ajv({
-    "verbose": true,
-    "allErrors": true
-});
+let ajv = new Ajv({ "verbose": true, "allErrors": true });
+let validate = ajv.compile(JSON.parse(fs.readFileSync(json_schema_path, 'utf8')));
 
+let mongoServerUrl = 'mongodb://138.197.6.26:27017/';
+let devDbName = "courseplannerdb-dev";
+let prodDbName = "courseplannerdb";
+let dbName = (argv.prod) ? prodDbName : devDbName;
+let dbFullUrl = mongoServerUrl + dbName;
 
-/**
- * constants
- */
-const DIR = '../../r/course-info-json-files/';
-const jsonFilenameRegex = /_document.json/;//filter by filename
-const mongoServerUrl = 'mongodb://138.197.6.26:27017/';
-const devDbName = "courseplannerdb-dev";
-const prodDbName = "courseplannerdb";
-const dbName = (argv.prod) ? prodDbName : devDbName;
-const dbFullUrl = mongoServerUrl + dbName;
+let log = "*** Course Info Validation Log ***<br><br>";
 
-/**
- * main method
- */
-let storeAllCourses = (function() {
+let storeAllCourseInfo = (function (){
 
-    console.log("Storing course data in database...");
+    console.log("Storing + validating sequence json data");
+
+    let numValidated = 0;
 
     MongoClient.connect(dbFullUrl, function(err, db) {
         assert.equal(null, err);
         console.log("Connected successfully to db server");
 
-        let filesPushed = 0, totalFiles = 0;
+        let foundIssue = false;
 
-        // read all files in Rscript output folder and push to DB
-        fs.readdir(DIR, function (err, files) {
-            let allDocuments = files.filter(function(filename){
-                return jsonFilenameRegex.test(filename);
-            });
-            allDocuments.forEach(function (file) {
-                totalFiles++;
-                fs.readFile(DIR + file, "utf-8", function (err, fileContent) {
-                    let courseJSON = JSON.parse(fileContent);
+        // read all files in sequence folder and pass them through the validator
+        fs.readdir(scraped_course_info_folder, function (err, files) {
+            files = files.filter(file => course_info_document_regex.test(file))
+            files.forEach(function (file) {
+                fs.readFile(scraped_course_info_folder + '/' + file, "utf-8", function (err, fileContent) {
                     if (err) {
                         throw err;
                     }
-                    console.log("Writing contents of file: " + file + " to db.");
-                    courseJSON.forEach(function(course){
+                    let courseInfosJSON = JSON.parse(fileContent);
+
+                    let isSequenceValid = validate(courseInfosJSON);
+
+                    numValidated++;
+                    if(!isSequenceValid){
+                        logMessage(file + ": FAIL - ");
+                        logMessage(JSON.stringify(validate.errors, undefined, 4));
+                        foundIssue = true;
+                    } else {
+                        logMessage(file + ": PASS");
+
                         // write the json to the db
-                        db.collection("courseData").update({_id : course.code}, {$set:course}, {upsert: true}, function(err, result) {
-                            assert.equal(err, null);
-                            filesPushed++;
-                            if(filesPushed == totalFiles){
-                                db.close();
-                            }
-                        });
-                    });
+                        courseInfosJSON.forEach(courseInfoJSON => {
+                            db.collection("courseData").update({_id : courseInfoJSON.code}, {$set:courseInfoJSON}, {upsert: true}, function(err, result) {
+                                assert.equal(err, null);
+                                logMessage("Wrote contents of file: " + courseInfoJSON.code + " to db.");
+                            });
+                        })
+                    }
+
+                    if (numValidated == files.length) {
+                        db.close();
+                        if(foundIssue){
+                            //sendIssueEmail();
+                        }
+                    }
                 });
             });
         });
     });
 })();
+
+function logMessage(message){
+    console.log(message);
+    log += message + "<br>"
+}
+
+function sendIssueEmail(){
+
+    let message = "The course info scraper encountered errors in its most recent execution (" + new Date().toString() + ")\n" +
+                    " Below are the logs from the scrape attempt:<br><br>";
+    message += log;
+
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // secure:true for port 465, secure:false for port 587
+        auth: {
+            user: 'concordiacourseplanner@gmail.com',
+            pass: 'tranzone'
+        }
+    });
+
+    let mailOptions = {
+        from: '"Course Planner Debug" <concordiacourseplanner@gmail.com>', // sender address
+        to: 'davidhuculak5@gmail.com, petergranitski@gmail.com , stumash1@gmail.com', // list of receivers
+        subject: 'Course Planner has encountered an issue', // Subject line
+        html: message // html body
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+    });
+}
