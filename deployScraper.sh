f265a01764eda5430cbd24e136e6245121fb8df1
Stuart Mashaal <stumash1@gmail.com>
2017-09-03 15:06:53 -0400

    fix deployScraper to account for webscraping changes

--- deployScraper.sh
+++ deployScraper.sh
@@ -1,1 +1,3 @@
-rsync -rav --exclude=node/node_modules --exclude=node/course-seq/sequences --exclude=r/course-info-jsonfiles webscraping/* david@138.197.6.26:/opt/cp-webscraping
+rsync -rav webscraping/* \
+    --exclude=node_modules --exclude=courseInfo/scrapedJson --exclude=courseSequences/scrapedJson \
+    david@138.197.6.26:/opt/cp-webscraping
