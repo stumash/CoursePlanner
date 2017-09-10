a8936785204ae5702e3f1bc1fc600a4ae2d94408
Stuart Mashaal <stumash1@gmail.com>
2017-09-10 18:21:01 -0400

    travisInstall.sh only contains 'npm install'

--- scripts/travisInstall.sh
+++ scripts/travisInstall.sh
@@ -1,10 +1,10 @@
 # install node and npm
-sudo apt-get install node.js
-sudo apt-get install npm
+#sudo apt-get install node.js
+#sudo apt-get install npm
 
 # install java and maven
-sudo apt-get install default-jdk
-sudo apt-get install maven
+#sudo apt-get install default-jdk
+#sudo apt-get install maven
 
 # install js libs
 npm install
