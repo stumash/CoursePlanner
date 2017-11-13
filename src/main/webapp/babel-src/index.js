c6063c8ab09d3b0991174bc510289f82440a8292
David Huculak <davidhuculak5@gmail.com>
2017-11-13 15:59:21 -0500

    refactored/cleaned up css

--- src/main/webapp/babel-src/index.js
+++ src/main/webapp/babel-src/index.js
@@ -8,18 +8,8 @@ const muiTheme = getMuiTheme({
     palette: {
         primary1Color: "#6c1540",
         primary2Color: "#3d001a",
-        // primary3Color: grey400,
         accent1Color: "#f5bb2b",
-        // accent2Color: grey100,
-        // accent3Color: grey500,
-        // textColor: darkBlack,
-        // alternateTextColor: white,
-        // canvasColor: white,
-        // borderColor: grey300,
-        // disabledColor: fade(darkBlack, 0.3),
         pickerHeaderColor: "#6c1540",
-        // clockCircleColor: fade(darkBlack, 0.07),
-        // shadowColor: fullBlack,
     }
 });
 
