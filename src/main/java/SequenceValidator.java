d8ae189f42a3d33afb08f4a11e1eb7d5f67089f2
stumash <stumash1@gmail.com>
2017-11-20 22:47:25 -0500

    add apache.commons.lang package, begin sequence validation logic
    The apache.commons.lang package has
    > `public String ExceptionUtils.getStackTrace(e)`
    which is used to send stack traces directly to the front end.
    
    First steps towards new validation algorithm.

--- src/main/java/SequenceValidator.java
+++ src/main/java/SequenceValidator.java
@@ -5,47 +5,57 @@ import org.json.JSONObject;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class SequenceValidator extends DBServlet {
+public class SequenceValidator extends CPServlet {
 
     private static HashMap<String,CourseInfo> courseInfoMap;
 
     public void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
+    throws ServletException, IOException
+    {
         logger.info("---------User requested a sequence validation---------");
 
         response.setContentType("text/html");
 
-        String responseString = "{}";
+        JSONObject courseSequenceObject = (JSONObject) grabPropertyFromRequest("courseSequenceObject", request);
 
-        logger.info("Responding with: " + responseString);
-        PrintWriter out = response.getWriter();
-        out.println(responseString);
-
-    }
-
-    private void logSemesterData(ArrayList<Semester> semesters){
-        for(Semester s:semesters){
-            logger.info("------------------------------");
-            logger.info(s.getSeason() + ":");
-            if(s.isWorkTerm()){
-                logger.info("Work Term");
-            }
-            for(Course c:s.getCourses()){
-                if(c.isElective()){
-                    logger.info(c.getElectiveType() + " elective");
-                } else {
-                    logger.info(c.getCode() + ", " + c.getName() + ", " + c.getCredits());
-                }
-            }
+        String validationResults;
+        try {
+            validationResults = validateSequence(courseSequenceObject).toString();
+        } catch(Exception e){
+            JSONObject validationResultsJsonObj = new JSONObject();
+            validationResultsJsonObj.put("error", ExceptionUtils.getStackTrace(e));
+            validationResults = validationResultsJsonObj.toString();
         }
+
+        logger.info("Responding with: " + validationResults);
+
+        response.getWriter().println(validationResults);
     }
 
-    private JSONObject validateSequence(ArrayList<Semester> semesters) throws JSONException {
+    /**
+     * @param cso the json course sequence object
+     * @return a json object representing the results of the course sequence validation
+     */
+    private JSONObject validateSequence(JSONObject cso)
+    throws Exception
+    {
+        HashMap<String, Integer> course2semester = new HashMap<>();
+        JSONArray yearlist = cso.getJSONArray("yearlist");
+        int semesterNum = -1;
+
+        for (JSONObject year : (JSONObject) yearlist) {
+            semesterNum++;
+
+            //TODO
+        }
+        return null;
     }
 }
