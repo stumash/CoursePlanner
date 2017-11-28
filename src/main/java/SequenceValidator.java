2e36765e396dc7ac84f5431abab547bb87c92f6b
stumash <stumash1@gmail.com>
2017-11-27 20:11:16 -0500

    getting closer

--- src/main/java/SequenceValidator.java
+++ src/main/java/SequenceValidator.java
@@ -12,19 +12,21 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.awt.Point;
 
 public class SequenceValidator extends CPServlet {
 
-    // Constants representing the types of issues that course sequences are validated against.
-    private static enum VALIDATION_ISSUES {
+    // Constants representing the types of issues that course sequences are validated against
+    private static enum ISSUES {
 
-        COURSE_DUPLICATE, CREDITCOUNT, PREREQUISITE, COREQUISITE;
+        REPEATED, CREDITCOUNT, PREREQUISITE, COREQUISITE;
 
         // JSON-friendly names for returning validation messages to client
         @Override
         public String toString() {
-            if (this == COURSE_DUPLICATE) {
-                return "courseDuplicate";
+            if (this == REPEATED) {
+                return "repeated";
             } else if (this == CREDITCOUNT) {
                 return "creditCount";
             } else if (this == PREREQUISITE) {
@@ -36,6 +38,32 @@ public class SequenceValidator extends CPServlet {
         }
     }
 
+    // Constants representing the types of warnings that course sequences are validated against
+    private static enum WARNINGS {
+
+        UNSELECTED_OPTION;
+
+        // JSON-friendly names for returning validation messages to client
+        @Override
+        public String toString() {
+            if (this == UNSELECTED_OPTION) {
+                return "unselectedOption";
+            }
+            return null;
+        }
+    }
+
+    // Constants representing the 3 possible seasons in which there can be a semester of courses
+    private static enum SEASONS {
+        FALL, WINTER, SUMMER;
+        // JSON-friendly
+        @Override
+        public String toString() {
+            return this.name().toLowerCase();
+        }
+    }
+
+    // respond to POST request
     public void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException
     {
@@ -65,7 +93,8 @@ public class SequenceValidator extends CPServlet {
     }
 
     /**
-     * @param cso the json course sequence object
+     * Perform the JSON course sequence validation
+     * @param cso the json course sequence object to validate
      * @return a json object representing the results of the course sequence validation
      */
     private JSONObject validateSequence(JSONObject cso)
@@ -74,17 +103,20 @@ public class SequenceValidator extends CPServlet {
         JSONObject sequenceValidationResults = new JSONObject();
         boolean sequenceIsValid = true;
         JSONArray issues = new JSONArray();
+        JSONArray warnings = new JSONArray();
 
         Double minTotalCredits = Double.parseDouble((String)cso.get("minTotalCredits"));
-        double creditCount = 0.0; // sum up all credits to check if total credits in valid range
+        Double creditCount = 0.0;
 
-        HashMap<String, Integer> cc2si = new HashMap<>(); // map course code to semester id
+        // set of course ids with fatal warnings/issues. course id = Point<unique semester id, course list index>
+        HashSet<Point> errci = new HashSet<>();
+        // map course codes to list of course ids in course sequence
+        HashMap<String, ArrayList<Point>> cc2ci = new HashMap<String, ArrayList<Point>>();
 
         JSONArray yearList = cso.getJSONArray("yearList");
         if (yearList.length() == 0) throw new Exception("empty yearList");
 
-        // loop through all the courses in each semester multiple times
-        int numLoops = 2; // for now only 2 loops needed
+        int numLoops = 2; // for now only 2 loops needed for all validation
         for (int loopCount = 0; loopCount < numLoops; loopCount++)
         {
             // for each year
@@ -92,42 +124,75 @@ public class SequenceValidator extends CPServlet {
             {
                 JSONObject year = (JSONObject) yearList.get(yearIdx);
 
-                JSONObject fall   = (JSONObject) year.get("fall");
-                JSONObject winter = (JSONObject) year.get("winter");
-                JSONObject summer = (JSONObject) year.get("summer");
+                JSONObject fall   = year.getJSONObject(SEASONS.FALL.toString());
+                JSONObject winter = year.getJSONObject(SEASONS.WINTER.toString());
+                JSONObject summer = year.getJSONObject(SEASONS.SUMMER.toString());
 
                 // for each semester in that year
                 JSONObject[] semsInYear = {fall, winter, summer};
-                for (int semsInYearIdx = 0; semsInYearIdx < semsInYear.length; semsInYearIdx++)
+                for (int semsInYearIdx = 0; semsInYearIdx < SEASONS.values().length; semsInYearIdx++)
                 {
-                    int uniqueSemesterId = yearIdx * semsInYear.length + semsInYearIdx;
+                    int uniqueSemesterId = yearIdx * SEASONS.values().length + semsInYearIdx; // for cc2ci
 
                     // for each course in that semester
-                    JSONArray coursesInSem = (JSONArray) semsInYear[semsInYearIdx].get("courseList");
+                    JSONArray coursesInSem = semsInYear[semsInYearIdx].getJSONArray("courseList");
                     for (int coursesInSemIdx = 0; coursesInSemIdx < coursesInSem.length(); coursesInSemIdx++)
                     {
-                        JSONObject course = (JSONObject) coursesInSem.get(coursesInSemIdx);
+
+                        // VALIDATE FATAL ISSUES/WARNINGS (THOSE THAT PREVENT FURTHER VALIDATION OF A COURSE)
+                        //------------------------------------------------------------------------------------
+
+                        // no matter what loopCount, stop validating this course if it already has a fatal error
+                        if (errci.contains(new Point(uniqueSemesterId, coursesInSemIdx))) {
+                            continue;
+                        }
+
+                        JSONObject course = null;
+                        // course can be single course or orList of courses
+                        try {
+                            course = (JSONObject) coursesInSem.get(coursesInSemIdx);
+                        } catch(JSONException e) {
+                            JSONArray orList = (JSONArray) coursesInSem.get(coursesInSemIdx);
+                            int optionsSelected = 0;
+                            for (int orListIdx = 0; orListIdx < orList.length(); orListIdx++) {
+                                JSONObject courseInOrList = (JSONObject) orList.get(orListIdx);
+                                if (courseInOrList.getBoolean("isSelected")) {
+                                    optionsSelected += 1;
+                                    course = courseInOrList;
+                                }
+                            }
+
+                            // UNSELECTED OPTION
+
+                            if (optionsSelected < 1) {
+                                sequenceIsValid = false;
+                                errci.contains(new Point(uniqueSemesterId, coursesInSemIdx));
+
+                                warnings
+                                .put(new JSONObject()
+                                    .put("type", WARNINGS.UNSELECTED_OPTION.toString())
+                                    .put("data", new JSONObject()
+                                        .put("position", positionObject(yearIdx,semsInYearIdx,coursesInSemIdx))
+                                    )
+                                );
+
+                                continue; // 'unselected option' is a fatal error so stop validating this course
+                            }
+                            // TODO: handle optionsSelected > 1
+                        }
+
+                        // VALIDATE REGULAR ISSUES/WARNINGS
+                        //----------------------------------
+
                         String courseCode = (String) course.get("code");
 
                         if (loopCount == 0)
                         {
-                            // COURSE DUPLICATE
-
-                            // if not duplicate course
-                            if (cc2si.get(courseCode) == null) {
-                                // only put course in map once, not for duplicates
-                                cc2si.put(courseCode, uniqueSemesterId);
+                            if (cc2ci.get(courseCode) == null) {
+                                ArrayList<Point> courseOccurences = new ArrayList<>();
+                                courseOccurences.add(new Point(uniqueSemesterId, coursesInSemIdx));
                             } else {
-                                sequenceIsValid = false;
-
-                                JSONObject issue = new JSONObject();
-                                issue.put("type", VALIDATION_ISSUES.COURSE_DUPLICATE.toString());
-
-                                JSONObject issueData = new JSONObject();
-                                issueData.put("courseCode", courseCode);
-
-                                issue.put("data", issueData);
-                                issues.put(issue);
+                                cc2ci.get(courseCode).add(new Point(uniqueSemesterId, coursesInSemIdx));
                             }
 
                             // CREDIT COUNT
@@ -136,64 +201,42 @@ public class SequenceValidator extends CPServlet {
                         }
                         else if (loopCount == 1)
                         {
-                            // PREREQUISITE
+                            // TODO: PREREQUISITE
 
                             boolean prereqsValid = true;
 
-                            JSONObject issue = new JSONObject();
-                            issue.put("type", VALIDATION_ISSUES.PREREQUISITE.toString());
-
-                            JSONObject issueData = new JSONObject();
-                            issueData.put("culpritCourseCode", courseCode);
-                            JSONArray missingPrereqCourseCodes = new JSONArray();
-
-                            JSONArray prereqs = (JSONArray) course.get("prereqs");
-                            for (int prereqIdx = 0; prereqIdx < prereqs.length(); prereqIdx++) {
-                                String prereqCourseCode = (String) prereqs.get(prereqIdx);
-                                Integer prereqSemesterId = cc2si.get(prereqCourseCode);
-                                if (prereqSemesterId == null || prereqSemesterId >= uniqueSemesterId) {
-                                    sequenceIsValid = false;
-                                    prereqsValid = false;
-                                    missingPrereqCourseCodes.put(prereqCourseCode);
-                                }
-                            }
-
-                            if (!prereqsValid) {
-                                issueData.put("missingCourseCodes", missingPrereqCourseCodes);
-                                issue.put("data", issueData);
-                                issues.put(issue);
-                            }
-
-                            // COREQUISITE
+                            // TODO: COREQUISITE
 
                             boolean coreqsValid = true;
-
-                            issue = new JSONObject();
-                            issue.put("type", VALIDATION_ISSUES.COREQUISITE.toString());
-
-                            issueData = new JSONObject();
-                            issueData.put("culpritCourseCode", courseCode);
-                            JSONArray missingCoreqCourseCodes = new JSONArray();
-
-                            JSONArray coreqs = (JSONArray) course.get("coreqs");
-                            for (int coreqIdx = 0; coreqIdx < coreqs.length(); coreqIdx++) {
-                                String coreqCourseCode = (String) coreqs.get(coreqIdx);
-                                Integer coreqSemesterId = cc2si.get(coreqCourseCode);
-                                if (coreqSemesterId == null || coreqSemesterId > uniqueSemesterId) {
-                                    sequenceIsValid = false;
-                                    coreqsValid = false;
-                                    missingCoreqCourseCodes.put(coreqCourseCode);
-                                }
-                            }
-
-                            if (!coreqsValid) {
-                                issueData.put("missingCourseCodes", missingCoreqCourseCodes);
-                                issue.put("data", issueData);
-                                issues.put(issue);
-                            }
-                        }
+                            //
+                       }
                     }
                 }
+            }
+        }
+
+        // REPEATED
+
+        for (String courseCode : cc2ci.keySet()) {
+            ArrayList<Point> courseOccurences = cc2ci.get(courseCode);
+            if (courseOccurences.size() > 1) {
+                sequenceIsValid = false;
+
+                JSONArray positions = new JSONArray();
+                for (Point courseOccurencePosition : courseOccurences) {
+                    int uniqueSemesterId = (int) courseOccurencePosition.getX(); // no precision lost since
+                    int courseIndex = (int) courseOccurencePosition.getY(); // double has 52 bits and int has 32
+                    positions.put(positionObject(uniqueSemesterId, courseIndex));
+                }
+
+                issues
+                .put(new JSONObject()
+                    .put("type", ISSUES.REPEATED.toString())
+                    .put("data", new JSONObject()
+                        .put("courseCode", courseCode)
+                        .put("positions", positions)
+                    )
+                );
             }
         }
 
@@ -202,26 +245,41 @@ public class SequenceValidator extends CPServlet {
         if (creditCount < minTotalCredits) {
             sequenceIsValid = false;
 
-            JSONObject issue = new JSONObject();
-            issue.put("type", VALIDATION_ISSUES.CREDITCOUNT.toString());
-
-            JSONObject issueData = new JSONObject();
-            issueData.put("required", minTotalCredits.toString());
-            issueData.put("actual", new Double(creditCount).toString());
-
-            issue.put("data", issueData);
-            issues.put(issue);
+            issues
+            .put(new JSONObject()
+                .put("type", ISSUES.CREDITCOUNT.toString())
+                .put("data", new JSONObject()
+                    .put("required", minTotalCredits.toString())
+                    .put("actual", creditCount.toString())
+                )
+            );
         }
 
         // SEND ISSUE LIST IF SEQUENCE NOT VALID
 
         if (!sequenceIsValid) {
-            sequenceValidationResults.put("isValid", "false");
-            sequenceValidationResults.put("issues", issues);
+            sequenceValidationResults
+            .put("isValid", "false")
+            .put("issues", issues)
+            .put("warnings", warnings);
         } else {
             sequenceValidationResults.put("isValid", "true");
         }
 
         return sequenceValidationResults;
+    }
+
+    // helpers to create JSON object representing position of course in course sequence
+    private JSONObject positionObject(Integer yearIdx, Integer seasonIdx, Integer courseIdx) {
+        return new JSONObject()
+            .put("yearIndex", yearIdx.toString())
+            .put("season", SEASONS.values()[seasonIdx].toString())
+            .put("courseIndex", courseIdx.toString());
+    }
+    private JSONObject positionObject(Integer uniqueSemesterId, Integer courseIdx) {
+        return new JSONObject()
+            .put("yearIndex", new Integer(uniqueSemesterId / SEASONS.values().length).toString())
+            .put("season", SEASONS.values()[uniqueSemesterId % SEASONS.values().length].toString())
+            .put("courseIndex", courseIdx.toString());
     }
 }
